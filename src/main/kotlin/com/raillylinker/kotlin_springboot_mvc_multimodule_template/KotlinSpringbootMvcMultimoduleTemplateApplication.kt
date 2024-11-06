24cd59c87f95b48dd346b1a00851e1994c8e2013
RaillyLinker <raillylinker@gmail.com>
2024-11-06 21:50:50 +0900

    feat : init project

---
+++ src/main/kotlin/com/raillylinker/kotlin_springboot_mvc_multimodule_template/KotlinSpringbootMvcMultimoduleTemplateApplication.kt
@@ -0,0 +1,11 @@
+package com.raillylinker.kotlin_springboot_mvc_multimodule_template
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringbootMvcMultimoduleTemplateApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringbootMvcMultimoduleTemplateApplication>(*args)
+}
---
+++ src/test/kotlin/com/raillylinker/kotlin_springboot_mvc_multimodule_template/KotlinSpringbootMvcMultimoduleTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.raillylinker.kotlin_springboot_mvc_multimodule_template
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringbootMvcMultimoduleTemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
