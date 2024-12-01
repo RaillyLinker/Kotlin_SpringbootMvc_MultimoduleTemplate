75cbd3a050cc686b0b1477922a43fff31fae71d1
RaillyLinker <raillylinker@gmail.com>
2024-12-01 12:42:56 +0900

    refactor : 사용하지 않는 모듈 의존성 제거

--- module-quartz/build.gradle.kts
+++ module-quartz/build.gradle.kts
@@ -5,7 +5,6 @@ version = "0.0.1-SNAPSHOT"
 
 dependencies {
     // (모듈)
-    implementation(project(":module-jpa"))
 
     // (Spring Quartz)
     implementation("org.springframework.boot:spring-boot-starter-quartz:3.3.5")
